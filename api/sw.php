4db683d5ce13f1578261292fbd33ac1cf543fef2
staybreeze <wunshengliao@gmail.com>
2024-01-11 15:36:45 +0800

    feat:完成預告片的排序按鈕功能

---
+++ api/sw.php
@@ -0,0 +1,17 @@
+<?php
+include_once "db.php";
+
+// $table=$_POST['table'];
+$DB=new DB($_POST['table']);
+
+$row=$DB->find($_POST['id']);
+$sw=$DB->find($_POST['sw']);
+
+$tmp=$row['rank'];
+$row['rank']=$sw['rank'];
+$sw['rank']=$tmp;
+
+$DB->save($row);
+$DB->save($sw);
+
+?>
--- back/poster.php
+++ back/poster.php
@@ -35,8 +35,9 @@ foreach($pos as $idx => $po){
         <input type="text" name="name[]" value="<?=$po['name'];?>">
     </div>
     <div>
-        <input type="button" value="往上" data-id="<?=$po['id'];?>" data-sw="<?=($idx!==0)?$pos[$idx-1]['id']:$po['id'];?>">
-        <input type="button" value="往下" data-id="<?=$po['id'];?>" data-sw="<?=((count($pos)-1)!=$idx)?$pos[$idx+1]['id']:$po['id'];?>">
+        <input class="btn" type="button" value="往上" data-id="<?=$po['id'];?>" data-sw="<?=($idx!==0)?$pos[$idx-1]['id']:$po['id'];?>">
+        <!-- count($pos)是count($pos)所有的筆數 -->
+        <input class="btn" type="button" value="往下" data-id="<?=$po['id'];?>" data-sw="<?=((count($pos)-1)!=$idx)?$pos[$idx+1]['id']:$po['id'];?>">
     </div>
     <div style='color:black'>
         <input type="hidden" name="id[]" value="<?=$po['id'];?>">
@@ -78,3 +79,16 @@ foreach($pos as $idx => $po){
 </div>
 </form>
 </div>
+
+<script>
+
+$(".btn").on("click",function(){
+let id=$(this).data('id');
+let sw=$(this).data('sw');
+let table='poster';
+$.post("./api/sw.php",{id,sw,table},()=>{
+    location.reload();
+})
+})
+
+</script>
