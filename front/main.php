ee7d79658339f9f2036320d6c12c104d486ae845
staybreeze <wunshengliao@gmail.com>
2024-01-12 14:04:42 +0800

    feat:完成劇情簡介頁面

--- front/main.php
+++ front/main.php
@@ -1,26 +1,82 @@
-<div class="tab rb" style="width:87%;">
-  <div style="background:#FFF; width:100%; color:#333; text-align:left">
-    <video src="movie/03B20v.avi" width="300px" height="250px" controls="" style="float:right;"></video>
-    <font style="font-size:24px"> <img src="Profile page_files/03B20.png" width="200px" height="250px" style="margin:10px; float:left">
-      <p style="margin:3px">影片名稱 ：
-        <input type="button" value="線上訂票" onclick="lof(&#39;?do=ord&amp;id=4&#39;)" style="margin-left:50px; padding:2px 4px" class="b2_btu">
-      </p>
-      <p style="margin:3px">影片分級 ： <img src="Profile page_files/03C04.png" style="display:inline-block;">限制級 </p>
-      <p style="margin:3px">影片片長 ： 時/分</p>
-      <p style="margin:3px">上映日期 2014/02/14</p>
-      <p style="margin:3px">發行商 ： </p>
-      <p style="margin:3px">導演 ： </p>
-      <br>
-      <br>
-      <p style="margin:10px 3px 3px 3px; word-break:break-all"> 劇情簡介：<br>
-      </p>
-    </font>
-    <table width="100%" border="0">
-      <tbody>
-        <tr>
-          <td align="center"><input type="button" value="院線片清單" onclick="lof(&#39;?&#39;)"></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+<div class="half" style="vertical-align:top;">
+    <h1>預告片介紹</h1>
+    <div class="rb tab" style="width:95%;">
+        <div >
+            <ul class="lists">
+            </ul>
+            <ul class="controls">
+            </ul>
+        </div>
+    </div>
+</div>
+<style>
+.movies{
+    display:flex;
+    flex-wrap:wrap;
+}
+.movie{
+ display:flex;
+ flex-wrap:wrap;
+ box-sizing: border-box;
+ padding:2px;
+ margin:0.5%;
+ border:1px solid #ccc;
+ border-radius: 5px;
+ width:49%;
+}    
+</style>
+<div class="half">
+    <h1>院線片清單</h1>
+    <div class="rb tab" style="width:95%;">
+    <div class="movies">
+    <?php
+        $today=date("Y-m-d");
+        $ondate=date("Y-m-d",strtotime("-2 days"));
+        $total=$Movie->count(" where `ondate`>='$ondate'  && `ondate` <='$today'  && `sh`=1");
+        $div=4;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+        $movies=$Movie->all(" where `ondate`>='$ondate'  && `ondate` <='$today'  && `sh`=1 order by rank limit $start,$div");
+        foreach($movies as $movie){
+    ?>
+    <div class="movie">
+        <div style="width:35%">
+            <a href='?do=intro&id=<?=$movie['id'];?>'>
+                <img src="./img/<?=$movie['poster'];?>" style="width:60px;border:3px solid white">
+            </a>
+        </div>
+        <div style="width:65%">
+            <div><?=$movie['name'];?></div>
+            <div style="font-size:13px;">分級: <img src="./icon/03C0<?=$movie['level'];?>.png" style="width:20px"></div>
+            <div style="font-size:13px;">上映日期:<?=$movie['ondate'];?></div>
+        </div>
+        <div style="width:100%">
+            <button onclick="location.href='?do=intro&id=<?=$movie['id'];?>'">劇情介紹</button>
+            <button onclick="location.href='?do=order&id=<?=$movie['id'];?>'">線上訂票</button>
+        </div>
+    </div>
+    <?php
+        }
+    ?>
+    </div>
+    <div class="ct">
+        <?php
+            if($now-1>0){
+                $prev=$now-1;
+                echo "<a href='?p=$prev'> < </a>";
+            }
+
+            for($i=1 ;$i<=$pages ;$i++){
+                echo "<a href='?p=$i'> $i </a>";
+            }
+
+            if($now+1<=$pages){
+                $next=$now+1;
+                echo "<a href='?p=$next'> > </a>";
+            }
+        ?>
+
+    </div>
+    </div>
 </div>
