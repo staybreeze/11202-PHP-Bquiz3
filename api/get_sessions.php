ad13577240abfc6e943b8d3f5694702a975fde52
staybreeze <wunshengliao@gmail.com>
2024-01-15 15:37:10 +0800

    feat:增加計算剩餘座位數的後端語法

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -18,15 +18,36 @@
 
 
 
+// $movie=$_GET['movie'];
+// $date=$_GET['date'];
+// $H=date("G");
+// $start=($H>=14 && $date==date("Y-m-d"))?7-ceil((24-$H)/2):1;
+
+// for($i=$start;$i<=5;$i++){
+//     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
+// }
+
+ 
 $movie=$_GET['movie'];
+$movieName=$Movie->find($movie)['name'];
 $date=$_GET['date'];
 $H=date("G");
 $start=($H>=14 && $date==date("Y-m-d"))?7-ceil((24-$H)/2):1;
 
 for($i=$start;$i<=5;$i++){
-    echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
+    /***
+     * 1. 去資料表撈出電影,日期,場次的訂單
+     * 2. 根據訂單,計算座位數
+     * 3. 在迴圈使用20-座位數來取得剩餘座位數
+     */
+    $qt=$Order->sum('qt',['movie'=>$movieName,'date'=>$date,'session'=>$sess[$i]]);
+    $lave=20-$qt;
+    echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 $lave</option>";
 }
 
 ?>
+
+
+
 
 
