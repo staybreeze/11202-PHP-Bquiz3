4543e24a1eee9203b834407901caa0d57c730031
staybreeze <wunshengliao@gmail.com>
2024-01-15 14:45:06 +0800

    feat:依據當下時間計算可訂的場次

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -1,13 +1,20 @@
 <?php include_once 'db.php';
+// $movie=$_GET['movie'];
+// $date=$_GET['date'];
+
+// for($i=1;$i<=5;$i++){
+//     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
+// }
+
+
 $movie=$_GET['movie'];
 $date=$_GET['date'];
+$H=date("G");
+$start=($H<14)?1:7-ceil((24-$H)/2);
 
-for($i=1;$i<=5;$i++){
+for($i=$start;$i<=5;$i++){
     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
 }
 
-
-
-
-
 ?>
+
