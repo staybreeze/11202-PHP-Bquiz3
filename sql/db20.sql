8fc81a2d2b4f2ab1b5e13279faa073bd927a4ad3
staybreeze <wunshengliao@gmail.com>
2024-01-15 08:24:28 +0800

    chore:新增sql檔

---
+++ sql/db20.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-01-15 01:20:08
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db20`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `movie`
+--
+
+CREATE TABLE `movie` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `level` int(1) NOT NULL,
+  `length` int(3) NOT NULL,
+  `ondate` date NOT NULL,
+  `publish` text NOT NULL,
+  `director` text NOT NULL,
+  `trailer` text NOT NULL,
+  `poster` text NOT NULL,
+  `intro` text NOT NULL,
+  `sh` int(1) NOT NULL,
+  `rank` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `movie`
+--
+
+INSERT INTO `movie` (`id`, `name`, `level`, `length`, `ondate`, `publish`, `director`, `trailer`, `poster`, `intro`, `sh`, `rank`) VALUES
+(1, '111', 1, 0, '2024-01-12', '', '', '', 'adult.jpg', '', 1, 1),
+(3, '', 4, 0, '2024-01-11', '', '', '', 'cheetos1.jpg', '', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `poster`
+--
+
+CREATE TABLE `poster` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `img` text NOT NULL,
+  `sh` int(1) NOT NULL,
+  `rank` int(10) NOT NULL,
+  `ani` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `poster`
+--
+
+INSERT INTO `poster` (`id`, `name`, `img`, `sh`, `rank`, `ani`) VALUES
+(1, '咪咪', 'cheetos6.jpg', 0, 1, 2),
+(2, '132', 'cat2.jpeg', 1, 2, 1),
+(3, '', 'cat4.jpg', 1, 3, 1);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `poster`
+--
+ALTER TABLE `poster`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `poster`
+--
+ALTER TABLE `poster`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
