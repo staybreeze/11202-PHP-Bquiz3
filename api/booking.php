171002fd94dda01a576ed30421e1a5a3ac9ed854
staybreeze <wunshengliao@gmail.com>
2024-01-19 10:56:18 +0800

    feat:撰寫前端計算勾選座位及判斷是否超過四張票

--- api/booking.php
+++ api/booking.php
@@ -62,3 +62,24 @@ $session=$_GET['session'];
     <button>訂購</button>
 </div>
 </div>
+
+<script>
+let seats=new Array();
+
+$(".chk").on("change",function(){
+    if($(this).prop('checked')){
+        if(seats.length+1<=4){
+            seats.push($(this).val())
+        }else{
+            $(this).prop('checked',false)
+            alert("每個人只能勾選四張票")
+        }
+    }else{
+
+        seats.splice(seats.indexOf($(this).val()),1)
+    }
+    console.log(seats.length)
+    $("#tickets").text(seats.length)
+
+})  
+</script>
